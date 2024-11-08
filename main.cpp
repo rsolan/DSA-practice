03d103502469411d16adfb51ed0ffa6fe5ed4aeb
Reema S <144640434+rsolan@users.noreply.github.com>
2024-11-08 21:04:12 +0000

    basic ll

--- main.cpp
+++ main.cpp
@@ -1,3 +1,47 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+class Node{
+
+  public:
+    int data;
+    Node* next;
+
+    Node(){
+      this->next =NULL;
+    }
+    Node(int data){
+      this->data=data;
+      this->next=NULL;
+    }
+
+};
+
+void print(Node* head){
+  Node* temp = head;
+  while(temp!=NULL){
+    std::cout<<temp->data << " ";
+    temp=temp->next;
+  }
+}
+
+
+int main() { 
+  
+  std::cout << "Hello World!\n"; 
+
+  Node a;
+  Node* first = new Node(10);
+  Node* sec = new Node(20);
+  Node* third = new Node(30);
+  Node* fourth = new Node(40);
+
+  first->next=sec;
+  sec->next=third;
+  third->next=fourth;
+
+  Node* head = first;
+  print(head);
+  
+
+
+}
